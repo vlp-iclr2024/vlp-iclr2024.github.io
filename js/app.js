13654cf781b7defb3529deec27f622d76b01bcfb
vlp <vlp@iclr2024.com>
2023-10-04 16:06:31 -0400

    saving website progress

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ $(document).ready(function() {
         "areareal": 1,
         "areasim": 1,
         "fractal": 1,
+        "aloha": 1,
     }
 
     var vid_start_times = {
@@ -124,6 +125,21 @@ $(document).ready(function() {
             3: 4,
             4: 6,
         },
+        "aloha": {
+            1: 0,
+            2: 3,
+            3: 6,
+            4: 9,
+            5: 12,
+            6: 15,
+            7: 18,
+            8: 21,
+            9: 24,
+            10: 27,
+            11: 30,
+            12: 33,
+            13: 36,
+        },
     }
 
     var vid_end_times = {
@@ -212,6 +228,21 @@ $(document).ready(function() {
             2: 4,
             3: 6,
             4: 8,
+        },
+        "aloha": {
+            1: 3,
+            2: 6,
+            3: 9,
+            4: 12,
+            5: 15,
+            6: 18,
+            7: 21,
+            8: 24,
+            9: 27,
+            10: 30,
+            11: 33,
+            12: 36,
+            13: 39,
         },
     }
 
